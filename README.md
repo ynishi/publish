4fb7ea1ca1b757fbb6ce756fb4dccdf36122fd0f
Yutaka Nishimura <ytk.nishimura@gmail.com>
2017-08-26 20:30:28 +0900

    add func Publish

--- README.md
+++ README.md
@@ -3,4 +3,15 @@
 * Publish documents to multi web service.
 * Document block is human can manage, not raw log.
 
+## Usage
+
+* do as command
+```
+export PUBLISH_ENV_APIKEY
+export PUBLISH_ENV_ENDPOINT
+export PUBLISH_ENV_DOCPATH
+
+./publish
+```
+
 * License is Apache-2.0
--- publish_test.go
+++ publish_test.go
@@ -1,14 +1,77 @@
-package main
+package publish
 
-import "testing"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"strings"
+	"testing"
 
-func TestGreet(t *testing.T) {
+	"github.com/spf13/viper"
+)
 
-	testPerson := "world"
-	testMsg := greet(testPerson)
+type MockPublisher struct {
+	Publisher
+	Conf *viper.Viper
+}
 
-	msg :="hello, world!"
-	if msg != testMsg {
-		t.Fatalf("Greet Message not match.\n want: %q,\n have: %q\n", msg, testMsg)
+func (m *MockPublisher) Publish(r io.Reader) error {
+	return nil
+}
+
+var mockPublishers []Publisher
+var mockPublisher *MockPublisher
+
+func init() {
+	SetReader(strings.NewReader(`<html></html>`))
+	mockPublisher = &MockPublisher{
+		Conf: viper.New(),
+	}
+	mockPublisher.Conf.Set("apikey", "test")
+	mockPublishers = []Publisher{mockPublisher}
+}
+
+func TestPublisher(t *testing.T) {
+	conf := viper.New()
+	conf.Set("apikey", "test")
+	if !reflect.DeepEqual(mockPublisher.Conf, conf) {
+		t.Fatalf("Failed match reader.\n want: %q,\n have: %q\n", conf, mockPublisher.Conf)
+	}
+	err := mockPublisher.Publish(reader)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestPublish(t *testing.T) {
+	err := Publish(mockPublishers)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestSetReader(t *testing.T) {
+	r := strings.NewReader(`test`)
+	SetReader(r)
+	if !reflect.DeepEqual(reader, r) {
+		t.Fatalf("Failed match reader.\n want: %q,\n have: %q\n", r, reader)
+	}
+}
+
+var githubHandler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `{}`)
+})
+
+func TestPublishGitHub(t *testing.T) {
+	ts := httptest.NewServer(githubHandler)
+	publishGitHub := &PublishGitHub{
+		Conf: viper.New(),
+	}
+	publishGitHub.Conf.Set("endpoint", ts.URL)
+	err := publishGitHub.Publish(reader)
+	if err != nil {
+		t.Fatal(err)
 	}
 }
