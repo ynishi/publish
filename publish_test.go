9b82dbc354ff2fb6e77dea84a483c0d655f62a07
Yutaka Nishimura <ytk.nishimura@gmail.com>
2017-08-28 22:17:15 +0900

    update license comment

--- publish_test.go
+++ publish_test.go
@@ -1,3 +1,7 @@
+// Copyright 2017 Yutaka Nishimura. All rights reserved.
+// Use of this source code is governed by a Apache License 2.0
+// license that can be found in the LICENSE file.
+
 package publish
 
 import (
